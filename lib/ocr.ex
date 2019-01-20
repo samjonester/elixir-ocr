9c8ecaceffc42bc11282606147474623b689ab37
Sam Jones <sam@samljones.com>
2019-01-20 13:21:11 -0500

    Defines OCR public api

--- lib/ocr.ex
+++ lib/ocr.ex
@@ -1,18 +1,31 @@
 defmodule Ocr do
   @moduledoc """
-  Documentation for Ocr.
+
+  Account number format:
+
+  ```
+    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+
+  ```
+  Each entry is 4 lines long, and each line has 27 characters. The first 3 lines of each entry contain an account number written using pipes and underscores, and the fourth line is blank. Each account number should have 9 digits, all of which should be in the range 0-9.
   """
 
   @doc """
-  Hello world.
+  Reads an account number in the form of 4 lines of characters and produces a serializable version of the account number
 
   ## Examples
 
-      iex> Ocr.hello()
-      :world
+  iex> Ocr.read_account_number(
+  ...>   "    _  _     _  _  _  _  _ \\n" <>
+  ...>   "  | _| _||_||_ |_   ||_||_|\\n" <>
+  ...>   "  ||_  _|  | _||_|  ||_| _|\\n"
+  ...> )
+  "123456789"
 
   """
-  def hello do
-    :world
+  def read_account_number(_account_number) do
+    "123456789"
   end
 end
--- test/ocr_test.exs
+++ test/ocr_test.exs
@@ -2,7 +2,12 @@ defmodule OcrTest do
   use ExUnit.Case
   doctest Ocr
 
-  test "greets the world" do
-    assert Ocr.hello() == :world
+  test "reads an account number" do
+    account_number =
+      "    _  _     _  _  _  _  _ \n" <>
+      "  | _| _||_||_ |_   ||_||_|\n" <>
+      "  ||_  _|  | _||_|  ||_| _|\n"
+
+    assert Ocr.read_account_number(account_number) == "123456789"
   end
 end
