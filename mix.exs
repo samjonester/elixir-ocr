0be6a3e889bfe1cebf74a29de74977d32aad7995
Dayton Nolan <me@daytonnolan.com>
2019-01-25 13:28:41 -0600

    Add converalls config

--- mix.exs
+++ mix.exs
@@ -9,6 +9,13 @@ defmodule Ocr.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [flags: [:unmatched_returns, :error_handling, :underspecs]],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       aliases: aliases()
     ]
   end
@@ -24,7 +31,8 @@ defmodule Ocr.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
